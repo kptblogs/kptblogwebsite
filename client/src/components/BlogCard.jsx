7ce33fa60ae443fe70b8cd6920b5bf2043f77283
kptudyogamela <kptudyogamela@gmail.com>
2025-08-16 06:33:08 +0530

    updated author nam

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -9,7 +9,7 @@ const BlogCard = ({ blog }) => {
       onClick={() => navigate(`/blog/${_id}`)}
       className="w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer"
     >
-      <img src={image} alt=" " className="aspect-video" />
+      <img src={image} alt=" blog image" className="aspect-video" />
       <span className="ml-5 mt-4 px-3 py-1 inline-block bg-primary bg-primary/20 rounded-full text-primary text-xs">
         {category}
       </span>
